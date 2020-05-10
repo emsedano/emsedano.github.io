a3360c582764ffe3363120258301f93c0503b52e
Elias Martinez <emsedano@live.com.mx>
2020-05-10 14:16:42 -0500

    Updates

---
+++ precache-manifest.ae4fd25a56ca3a6f09dc0572dea100f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50d0c1a7b20177b8f54236e0fc84d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49b5b887cceb410d8b49",
+    "url": "/static/css/main.ad2f6c20.chunk.css"
+  },
+  {
+    "revision": "0ea1d02cf827309132cf",
+    "url": "/static/js/2.41c01d4f.chunk.js"
+  },
+  {
+    "revision": "634cf502792dad31b11184de4a780f41",
+    "url": "/static/js/2.41c01d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b5b887cceb410d8b49",
+    "url": "/static/js/main.468035f8.chunk.js"
+  },
+  {
+    "revision": "035c8c409ab045f03eb4",
+    "url": "/static/js/runtime-main.efed7f1a.js"
+  },
+  {
+    "revision": "59e410d6b5769d7156d3f40749381e39",
+    "url": "/static/media/logo.59e410d6.svg"
+  }
+]);
